34cbd079a0f084a5aade1ed7993e0f391f805066
NoPro <haonp123@gmail.com>
2024-11-18 22:11:50 +0700

    configure cross origin backend

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [""],
+    origin: ["https://mern-auth-system-frontend.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
