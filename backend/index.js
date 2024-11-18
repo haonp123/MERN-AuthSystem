c1bb64652d94c6223bc090c553d6caa24d9bb3b4
NoPro <haonp123@gmail.com>
2024-11-18 22:03:42 +0700

    configure vercel backend

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,13 @@ const PORT = process.env.PORT || 5000;
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-
+app.use(
+  cors({
+    origin: [""],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
